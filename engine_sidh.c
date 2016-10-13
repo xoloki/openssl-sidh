dec6dbcee518bb70ee047530dd7d81e2ec54e940
Joey Yandle <dragon@dancingdragon.be>
2016-10-13 00:29:18 -0700

    basic engine support via gost

--- engine_sidh.c
+++ engine_sidh.c
@@ -7,15 +7,31 @@
 
 #define NID_id_SIDH 2560
 
-static EVP_PKEY_METHOD *pkey_sidh = NULL;
+static const char *engine_id = "sidh";
+static const char *engine_name = "A openssl engine for SIDH, a post-quantum public key protocol";
 
-static void init(void)
-{
-};
+static EVP_PKEY_METHOD *pkey_sidh = NULL;
 
 static int sidh_pkey_meth_nids[] = {
     NID_id_SIDH,
     0
+};
+
+static void init(void);
+static int sidh_pkey_meths(ENGINE *e, EVP_PKEY_METHOD **pmeth, const int **nids, int nid);
+static int register_pkey_meth(int id, EVP_PKEY_METHOD **pmeth, int flags);
+static int pkey_sidh_ctrl(EVP_PKEY_CTX *ctx, int type, int p1, void *p2);
+static int pkey_sidh_ec_ctrl(EVP_PKEY_CTX *ctx, const char *type, const char *value);
+
+int sidh_control_func(ENGINE *e, int cmd, long i, void *p, void (*f) (void));
+
+static void init(void)
+{
+    if(!register_pkey_meth(NID_id_SIDH, &pkey_sidh, 0)) {
+        fprintf(stderr, "SIDH engine failed to register id %d\n", NID_id_SIDH);
+    }
+
+    //fprintf(stderr, "SIDH engine registered all ids\n");
 };
 
 static int sidh_pkey_meths(ENGINE *e, EVP_PKEY_METHOD **pmeth, const int **nids, int nid)
@@ -37,11 +53,59 @@ static int sidh_pkey_meths(ENGINE *e, EVP_PKEY_METHOD **pmeth, const int **nids,
     return 0;
 }
                            
+static int register_pkey_meth(int id, EVP_PKEY_METHOD **pmeth, int flags)
+{
+    *pmeth = EVP_PKEY_meth_new(id, flags);
+    if (!*pmeth)
+        return 0;
 
-static const char *engine_id = "sidh";
-static const char *engine_name = "A openssl engine for SIDH, a post-quantum public key protocol";
+    switch (id) {
+    case NID_id_SIDH:
+        EVP_PKEY_meth_set_ctrl(*pmeth, pkey_sidh_ctrl, pkey_sidh_ec_ctrl);
+                               
+        //EVP_PKEY_meth_set_keygen(*pmeth, NULL, pkey_gost2001cp_keygen);
+        //EVP_PKEY_meth_set_derive(*pmeth, pkey_gost_derive_init, pkey_gost_ec_derive);
+        //EVP_PKEY_meth_set_paramgen(*pmeth, pkey_gost_paramgen_init, pkey_gost2001_paramgen);
+                                   
+
+
+        //EVP_PKEY_meth_set_init(*pmeth, pkey_gost_init);
+        //EVP_PKEY_meth_set_cleanup(*pmeth, pkey_gost_cleanup);
+        
+        //EVP_PKEY_meth_set_copy(*pmeth, pkey_gost_copy);
+
+        //EVP_PKEY_meth_set_sign(*pmeth, NULL, pkey_gost_ec_cp_sign);
+        //EVP_PKEY_meth_set_verify(*pmeth, NULL, pkey_gost_ec_cp_verify);
+        //EVP_PKEY_meth_set_encrypt(*pmeth, pkey_gost_encrypt_init, pkey_GOST_ECcp_encrypt);
+        //EVP_PKEY_meth_set_decrypt(*pmeth, NULL, pkey_GOST_ECcp_decrypt);
+
+    default:                   /* Unsupported method */
+        return 0;
+    }
+
+    return 1;
+}
+
+int sidh_control_func(ENGINE *e, int cmd, long i, void *p, void (*f) (void))
+{
+    return -1;
+}
+
+static int pkey_sidh_ctrl(EVP_PKEY_CTX *ctx, int type, int p1, void *p2)
+{
+    return 0;
+}
+
+static int pkey_sidh_ec_ctrl(EVP_PKEY_CTX *ctx, const char *type, const char *value)
+{
+    return 0;
+}
+
+
 static int bind(ENGINE *e, const char *id)
 {
+  fprintf(stderr, "SIDH engine binding...\n");
+
   int ret = 0;
 
   static int loaded = 0;
